a51ee1219cd6cac4d388e861b10fe62ef412b7bd
mkloubert <generalpd@gmx.de>
2014-05-30 21:55:24 +0200

    - ADD: proxy builder types

---
+++ MarcelJoachimKloubert.CLRToolboxReloaded.NET4_0/Objects/ObjectFactory.cs
@@ -0,0 +1,275 @@
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. https://github.com/mkloubert/CLRToolboxReloaded
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace MarcelJoachimKloubert.CLRToolbox.Objects
+{
+    /// <summary>
+    /// A class for building (dynamic / proxy) object.
+    /// </summary>
+    public sealed class ObjectFactory : ObjectBase
+    {
+        #region Fields (3)
+
+        private static ObjectFactory _instance;
+        private readonly IDictionary<Type, Type> _INTERFACE_PROXY_TYPES = new Dictionary<Type, Type>();
+        private readonly ModuleBuilder _MOD_BUILDER;
+
+        #endregion Fields
+
+        #region Constructors (8)
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObjectFactory" /> class.
+        /// </summary>
+        /// <param name="domain">The underlying app domain.</param>
+        /// <param name="asm">The assembly from where to take the name for the dynamic assembly from.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="domain" /> is a <see langword="null" /> references.
+        /// </exception>
+        /// <exception cref="NullReferenceException">
+        /// <paramref name="asm" /> is a <see langword="null" /> reference.
+        /// </exception>
+        public ObjectFactory(AppDomain domain, Assembly asm)
+            : this(domain, asm.GetName())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObjectFactory" /> class.
+        /// </summary>
+        /// <param name="domain">The underlying app domain.</param>
+        /// <param name="asmName">The name of the dynamic assembly.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="asmName" /> is a <see langword="null" /> reference.
+        /// </exception>
+        /// <exception cref="NullReferenceException">
+        /// <paramref name="domain" /> is a <see langword="null" /> reference.
+        /// </exception>
+        public ObjectFactory(AppDomain domain, AssemblyName asmName)
+            : this(domain.DefineDynamicAssembly(asmName, AssemblyBuilderAccess.RunAndSave))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObjectFactory" /> class.
+        /// </summary>
+        /// <param name="modBuilder">The value for the <see cref="ObjectFactory.ModuleBuilder" /> property.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="modBuilder" /> is a <see langword="null" /> references.
+        /// </exception>
+        public ObjectFactory(ModuleBuilder modBuilder)
+        {
+            if (modBuilder == null)
+            {
+                throw new ArgumentNullException("modBuilder");
+            }
+
+            this._MOD_BUILDER = modBuilder;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObjectFactory" /> class.
+        /// </summary>
+        /// <param name="asmBuilder">The assembly builder to use.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="asmBuilder" /> is a <see langword="null" /> references.
+        /// </exception>
+        public ObjectFactory(AssemblyBuilder asmBuilder)
+        {
+            if (asmBuilder == null)
+            {
+                throw new ArgumentNullException("asmBuilder");
+            }
+
+            this._MOD_BUILDER = asmBuilder.DefineDynamicModule(string.Format("TMDynamicObjectFactoryModule_{0:N}_{1}",
+                                                                             Guid.NewGuid(),
+                                                                             this.GetHashCode()));
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObjectFactory" /> class.
+        /// </summary>
+        /// <param name="asm">The assembly from where to take the name for the dynamic assembly from.</param>
+        /// <exception cref="NullReferenceException">
+        /// <paramref name="asm" /> is a <see langword="null" /> reference.
+        /// </exception>
+        public ObjectFactory(Assembly asm)
+            : this(AppDomain.CurrentDomain, asm)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObjectFactory" /> class.
+        /// </summary>
+        /// <param name="asmName">The name of the dynamic assembly.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="asmName" /> is a <see langword="null" /> reference.
+        /// </exception>
+        public ObjectFactory(AssemblyName asmName)
+            : this(AppDomain.CurrentDomain, asmName)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObjectFactory" /> class.
+        /// </summary>
+        /// <param name="domain">The underlying app domain.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="domain" /> is a <see langword="null" /> reference.
+        /// </exception>
+        public ObjectFactory(AppDomain domain)
+            : this(domain, Assembly.GetCallingAssembly().GetName())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObjectFactory" /> class.
+        /// </summary>
+        public ObjectFactory()
+            : this(AppDomain.CurrentDomain, Assembly.GetCallingAssembly().GetName())
+        {
+        }
+
+        #endregion Constructors
+
+        #region Properties (2)
+
+        /// <summary>
+        /// Gets the default singleton instance of that class.
+        /// </summary>
+        public static ObjectFactory Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ObjectFactory(AppDomain.CurrentDomain,
+                                                  Assembly.GetExecutingAssembly().GetName());
+                }
+
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Gets the underlying module builder.
+        /// </summary>
+        public ModuleBuilder ModuleBuilder
+        {
+            get { return this._MOD_BUILDER; }
+        }
+
+        #endregion Properties
+
+        #region Methods (5)
+
+        // Public Methods (4) 
+
+        /// <summary>
+        /// Creates a new proxy object for an interface type.
+        /// </summary>
+        /// <typeparam name="T">Type of the interface.</typeparam>
+        /// <returns>The proxy object.</returns>
+        /// <exception cref="ArgumentException">
+        /// <typeparamref name="T" /> represents no interface.
+        /// </exception>
+        public T CreateProxyForInterface<T>()
+        {
+            return (T)this.CreateProxyForInterface(typeof(T));
+        }
+
+        /// <summary>
+        /// Creates a new proxy object for an interface type.
+        /// </summary>
+        /// <param name="interfaceType">Type of the interface.</param>
+        /// <returns>The proxy object.</returns>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="interfaceType" /> represents no interface.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="interfaceType" /> is a <see langword="null" /> reference.
+        /// </exception>
+        public object CreateProxyForInterface(Type interfaceType)
+        {
+            var propxyType = this.GetProxyTypeForInterface(interfaceType);
+
+            return Activator.CreateInstance(propxyType);
+        }
+
+        /// <summary>
+        /// Returns the proxy type for an interface type.
+        /// </summary>
+        /// <typeparam name="T">Type of the interface.</typeparam>
+        /// <returns>The proxy type.</returns>
+        /// <exception cref="ArgumentException">
+        /// <typeparamref name="T" /> represents no interface.
+        /// </exception>
+        public Type GetProxyTypeForInterface<T>()
+        {
+            return this.GetProxyTypeForInterface(typeof(T));
+        }
+
+        /// <summary>
+        /// Returns the proxy type for an interface type.
+        /// </summary>
+        /// <param name="interfaceType">Type of the interface.</param>
+        /// <returns>The proxy type.</returns>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="interfaceType" /> represents no interface.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="interfaceType" /> is a <see langword="null" /> reference.
+        /// </exception>
+        public Type GetProxyTypeForInterface(Type interfaceType)
+        {
+            Type result;
+
+            lock (this._SYNC)
+            {
+                if (interfaceType == null)
+                {
+                    throw new ArgumentNullException("interfaceType");
+                }
+
+                if (interfaceType.IsInterface == false)
+                {
+                    throw new ArgumentException("interfaceType");
+                }
+
+                if (this._INTERFACE_PROXY_TYPES.TryGetValue(interfaceType, out result) == false)
+                {
+                    result = this.CreateInterfaceImplementation(interfaceType);
+
+                    this._INTERFACE_PROXY_TYPES
+                        .Add(interfaceType, result);
+                }
+            }
+
+            return result;
+        }
+
+        // Private Methods (1) 
+
+        private Type CreateInterfaceImplementation(Type interfaceType)
+        {
+            var genericPBType = typeof(ProxyBuilder<>);
+            var pbType = genericPBType.MakeGenericType(interfaceType);
+
+            var pb = Activator.CreateInstance(pbType);
+
+            return (Type)pbType.InvokeMember("CreateType",
+                                             BindingFlags.Public | BindingFlags.InvokeMethod | BindingFlags.Instance,
+                                             null,
+                                             pb,
+                                             new object[] { this.ModuleBuilder });
+        }
+
+        #endregion Methods
+    }
+}
---
+++ MarcelJoachimKloubert.CLRToolboxReloaded.NET4_0/Objects/ProxyBuilder.cs
@@ -0,0 +1,261 @@
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. https://github.com/mkloubert/CLRToolboxReloaded
+
+using MarcelJoachimKloubert.CLRToolbox.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace MarcelJoachimKloubert.CLRToolbox.Objects
+{
+    /// <summary>
+    /// Class that builds proxy types and object for an interface.
+    /// </summary>
+    /// <typeparam name="T">Type of the interface.</typeparam>
+    public sealed class ProxyBuilder<T> : ObjectBase
+    {
+        #region Constructors (1)
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ProxyBuilder{T}" /> class.
+        /// </summary>
+        /// <exception cref="ArgumentException">
+        /// Generic type does not represent an interface type.
+        /// </exception>
+        public ProxyBuilder()
+        {
+            if (typeof(T).IsInterface == false)
+            {
+                throw new ArgumentException("T");
+            }
+        }
+
+        #endregion Constructors
+
+        #region Properties (1)
+
+        /// <summary>
+        /// Gets the interface <see cref="Type" />.
+        /// </summary>
+        public Type InterfaceType
+        {
+            get { return typeof(T); }
+        }
+
+        #endregion Properties
+
+        #region Delegates and Events (1)
+
+        // Delegates (1) 
+
+        /// <summary>
+        /// Describes a function or method that provides a name (or a part) by a
+        /// </summary>
+        /// <param name="type">The type the name (part) is (or should be) based on.</param>
+        /// <returns>The name (part).</returns>
+        public delegate IEnumerable<char> TypeNameProvider(Type type);
+
+        #endregion Delegates and Events
+
+        #region Methods (6)
+
+        private static void CollectProperties(ICollection<PropertyInfo> properties, Type type)
+        {
+            var handledTypes = new HashSet<Type>();
+            CollectProperties(properties, type, handledTypes);
+        }
+
+        private static void CollectProperties(ICollection<PropertyInfo> properties, Type type, ICollection<Type> handledTypes)
+        {
+            properties.AddRange(type.GetProperties());
+
+            type.GetInterfaces()
+                .ForEach(ctx =>
+                {
+                    CollectProperties(ctx.State.Properties, ctx.Item,
+                                      ctx.State.HandlesTypes);
+                }, actionState: new
+                {
+                    HandlesTypes = handledTypes,
+                    Properties = properties,
+                });
+        }
+
+        /// <summary>
+        /// Creates a propxy <see cref="Type" /> for the interface of <typeparamref name="T" />.
+        /// </summary>
+        /// <param name="modBuilder">The module builder to use.</param>
+        /// <returns>The created type object.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="modBuilder" /> is <see langword="null" />.
+        /// </exception>
+        public Type CreateType(ModuleBuilder modBuilder)
+        {
+            return this.CreateType(modBuilder,
+                                   proxyTypeNamePrefixProvider: (interfaceType) => "TMImplOf_");
+        }
+
+        /// <summary>
+        /// Creates a propxy <see cref="Type" /> for the interface of <typeparamref name="T" />.
+        /// </summary>
+        /// <param name="modBuilder">The module builder to use.</param>
+        /// <param name="proxyTypeNamePrefixProvider">The logic that returns the prefix for the full name of the proxy type to create.</param>
+        /// <returns>The created type object.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="modBuilder" /> is <see langword="null" />.
+        /// </exception>
+        public Type CreateType(ModuleBuilder modBuilder,
+                               TypeNameProvider proxyTypeNamePrefixProvider)
+        {
+            return this.CreateType(modBuilder,
+                                   proxyTypeNamePrefixProvider: proxyTypeNamePrefixProvider,
+                                   proxyTypeNameSuffixProvider: (interfaceType) => string.Format("_{0:N}_{1}",
+                                                                                                 Guid.NewGuid(),
+                                                                                                 this.GetHashCode()));
+        }
+
+        /// <summary>
+        /// Creates a propxy <see cref="Type" /> for the interface of <typeparamref name="T" />.
+        /// </summary>
+        /// <param name="modBuilder">The module builder to use.</param>
+        /// <param name="proxyTypeNamePrefixProvider">The logic that returns the prefix for the full name of the proxy type to create.</param>
+        /// <param name="proxyTypeNameSuffixProvider">The logic that returns the suffix for the full name of the proxy type to create.</param>
+        /// <returns>The created type object.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="modBuilder" /> is <see langword="null" />.
+        /// </exception>
+        public Type CreateType(ModuleBuilder modBuilder,
+                               TypeNameProvider proxyTypeNamePrefixProvider,
+                               TypeNameProvider proxyTypeNameSuffixProvider)
+        {
+            return this.CreateType(modBuilder,
+                                   proxyTypeNamePrefixProvider: proxyTypeNamePrefixProvider,
+                                   proxyTypeNameProvider: (interfaceType) => this.InterfaceType.Name,
+                                   proxyTypeNameSuffixProvider: proxyTypeNameSuffixProvider);
+        }
+
+        /// <summary>
+        /// Creates a propxy <see cref="Type" /> for the interface of <typeparamref name="T" />.
+        /// </summary>
+        /// <param name="modBuilder">The module builder to use.</param>
+        /// <param name="proxyTypeNamePrefixProvider">The logic that returns the prefix for the full name of the proxy type to create.</param>
+        /// <param name="proxyTypeNameProvider">The logic that returns the name part of the proxy type to create.</param>
+        /// <param name="proxyTypeNameSuffixProvider">The logic that returns the suffix for the full name of the proxy type to create.</param>
+        /// <returns>The created type object.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="modBuilder" /> is <see langword="null" />.
+        /// </exception>
+        public Type CreateType(ModuleBuilder modBuilder,
+                               TypeNameProvider proxyTypeNamePrefixProvider,
+                               TypeNameProvider proxyTypeNameProvider,
+                               TypeNameProvider proxyTypeNameSuffixProvider)
+        {
+            if (modBuilder == null)
+            {
+                throw new ArgumentNullException("modBuilder");
+            }
+
+            var prefix = proxyTypeNamePrefixProvider != null ? (proxyTypeNamePrefixProvider(this.InterfaceType).AsString() ?? string.Empty).Trim()
+                                                             : null;
+            var name = proxyTypeNameProvider != null ? (proxyTypeNameProvider(this.InterfaceType).AsString() ?? string.Empty).Trim()
+                                                     : null;
+            var suffix = proxyTypeNameSuffixProvider != null ? (proxyTypeNameSuffixProvider(this.InterfaceType).AsString() ?? string.Empty).Trim()
+                                                             : null;
+
+            var baseType = typeof(object);
+
+            var typeBuilder = modBuilder.DefineType(string.Format("{0}{1}{2}",
+                                                                  prefix, name, suffix),
+                                                    TypeAttributes.Public | TypeAttributes.Class,
+                                                    baseType);
+
+            typeBuilder.AddInterfaceImplementation(this.InterfaceType);
+
+            var properties = new List<PropertyInfo>();
+            CollectProperties(properties, this.InterfaceType);
+
+            // build properties
+            properties.ForEach(ctx =>
+                {
+                    var p = ctx.Item;
+                    var tb = ctx.State.TypeBuilder;
+
+                    var propertyName = p.Name;
+                    var propertyType = p.PropertyType;
+
+                    var propertyBuilder = tb.DefineProperty(propertyName,
+                                                            PropertyAttributes.None,
+                                                            propertyType,
+                                                            Type.EmptyTypes);
+
+                    var fieldName = char.ToLower(propertyName[0]) +
+                                    new string(propertyName.Skip(1).ToArray());
+
+                    var field = tb.DefineField("_" + fieldName,
+                                               propertyType,
+                                               FieldAttributes.Family);
+
+                    // getter
+                    {
+                        var methodBuilder = typeBuilder.DefineMethod("get_" + propertyName,
+                                                                     MethodAttributes.Public | MethodAttributes.Virtual,
+                                                                     propertyType,
+                                                                     Type.EmptyTypes);
+
+                        var ilGen = methodBuilder.GetILGenerator();
+
+                        ilGen.Emit(OpCodes.Ldarg_0);      // load "this"
+                        ilGen.Emit(OpCodes.Ldfld, field); // load the property's underlying field onto the stack
+                        ilGen.Emit(OpCodes.Ret);          // return the value on the stack
+
+                        propertyBuilder.SetGetMethod(methodBuilder);
+                    }
+
+                    // setter
+                    {
+                        var methodBuilder = typeBuilder.DefineMethod("set_" + propertyName,
+                                                                     MethodAttributes.Public | MethodAttributes.Virtual,
+                                                                     typeof(void),
+                                                                     new Type[] { propertyType });
+
+                        var ilGen = methodBuilder.GetILGenerator();
+
+                        ilGen.Emit(OpCodes.Ldarg_0);      // load "this"
+                        ilGen.Emit(OpCodes.Ldarg_1);      // load "value" onto the stack
+                        ilGen.Emit(OpCodes.Stfld, field); // set the field equal to the "value" on the stack
+                        ilGen.Emit(OpCodes.Ret);          // return nothing
+
+                        propertyBuilder.SetSetMethod(methodBuilder);
+                    }
+                }, actionState: new
+                {
+                    TypeBuilder = typeBuilder,
+                });
+
+            // constructor
+            {
+                var baseConstructor = baseType.GetConstructor(new Type[0]);
+
+                var constructorBuilder = typeBuilder.DefineConstructor(MethodAttributes.Public,
+                                                                       CallingConventions.Standard,
+                                                                       Type.EmptyTypes);
+
+                var ilGen = constructorBuilder.GetILGenerator();
+                ilGen.Emit(OpCodes.Ldarg_0);                  // load "this"
+                ilGen.Emit(OpCodes.Call, baseConstructor);    // call the base constructor
+
+                //TODO
+                // define initial values
+
+                ilGen.Emit(OpCodes.Ret);    // return nothing
+            }
+
+            return typeBuilder.CreateType();
+        }
+
+        #endregion Methods
+    }
+}
