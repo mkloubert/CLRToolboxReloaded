98e162bc488804f3ea90c69c667baffe81712752
mkloubert <generalpd@gmx.de>
2014-06-01 11:57:35 +0200

    - ADD: WinForms projects

---
+++ MarcelJoachimKloubert.CLRToolboxReloaded.NET4_0.WinForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. https://github.com/mkloubert/CLRToolboxReloaded
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CLR Toolbox Reloaded WinForms")]
+[assembly: AssemblyDescription("Class library for Windows Forms (.NET 4.0 and above)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Marcel Joachim Kloubert")]
+[assembly: AssemblyProduct("MarcelJoachimKloubert.CLRToolboxReloaded.WinForms")]
+[assembly: AssemblyCopyright("Copyright © 2014 Marcel Joachim Kloubert <marcel.kloubert@gmx.net>")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(true)]
+
+[assembly: Guid("F2877B0C-C313-4F64-89AF-30C042A4C530")]
+
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
