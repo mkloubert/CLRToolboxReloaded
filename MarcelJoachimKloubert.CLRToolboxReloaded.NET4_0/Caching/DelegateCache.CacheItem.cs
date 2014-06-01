fb80918585cb6c436801884951769638d32793ce
mkloubert <generalpd@gmx.de>
2014-06-01 11:10:45 +0200

    - ADD: DelegateCache classes

--- MarcelJoachimKloubert.CLRToolboxReloaded.NET4_0/Caching/DelegateCache.CacheItem.cs
+++ MarcelJoachimKloubert.CLRToolboxReloaded.NET4_0/Caching/DelegateCache.CacheItem.cs
@@ -1,11 +1,145 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. https://github.com/mkloubert/CLRToolboxReloaded
+
+using System;
 
 namespace MarcelJoachimKloubert.CLRToolbox.Caching
 {
     partial class DelegateCache
     {
+        #region Nested Classes (1)
+
+        private sealed class CachedItem : IEquatable<Delegate>
+        {
+            #region Fields (7)
+
+            private DateTimeOffset? _cachedUntil;
+            private bool _hasBeenInvoked;
+            private object _lastResult;
+            private readonly object _SYNC = new object();
+            internal TimeSpan? Timeout;
+            internal readonly Delegate DELEGATE;
+
+            #endregion Fields
+
+            #region Constructors (1)
+
+            internal CachedItem(Delegate @delegate, TimeSpan? timeout)
+            {
+                this.DELEGATE = @delegate;
+                this.Timeout = timeout;
+
+                this.UpdateCachedUntil();
+            }
+
+            #endregion Constructors
+
+            #region Properties (1)
+
+            internal bool HasExpired
+            {
+                get
+                {
+                    DateTimeOffset now = this.GetNow();
+
+                    DateTimeOffset? cu = this._cachedUntil;
+                    return cu.HasValue &&
+                           (cu.Value < now);
+                }
+            }
+
+            #endregion Properties
+
+            #region Methods (7)
+
+            // Public Methods (3) 
+
+            public bool Equals(Delegate other)
+            {
+                return object.Equals(this.DELEGATE, other);
+            }
+
+            public override bool Equals(object other)
+            {
+                if (other is Delegate)
+                {
+                    return this.Equals((Delegate)other);
+                }
+
+                return base.Equals(other);
+            }
+
+            public override int GetHashCode()
+            {
+                return this.DELEGATE
+                           .GetHashCode();
+            }
+
+            // Private Methods (2) 
+
+            private DateTimeOffset GetNow()
+            {
+                return DateTimeOffset.Now;
+            }
+
+            private void UpdateCachedUntil()
+            {
+                DateTimeOffset now = this.GetNow();
+
+                TimeSpan? to = this.Timeout;
+                if (to.HasValue)
+                {
+                    this._cachedUntil = now.Add(to.Value);
+                }
+                else
+                {
+                    this._cachedUntil = null;
+                }
+            }
+
+            // Internal Methods (2) 
+
+            internal object Invoke(params object[] args)
+            {
+                object result;
+
+                lock (this._SYNC)
+                {
+                    if (this.HasExpired ||
+                        (this._hasBeenInvoked == false))
+                    {
+                        this._hasBeenInvoked = true;
+                        
+                        this._lastResult = this.DELEGATE
+                                               .Method
+                                               .Invoke(this.DELEGATE.Target,
+                                                       args ?? new object[] { null });
+
+                        this.UpdateCachedUntil();
+                    }
+
+                    result = this._lastResult;
+                }
+
+                return result;
+            }
+
+            internal bool Reset()
+            {
+                bool result;
+
+                lock (this._SYNC)
+                {
+                    result = !(this._hasBeenInvoked = false);
+                }
+
+                return result;
+            }
+
+            #endregion Methods
+        }
+
+        #endregion Nested Classes
     }
 }
--- MarcelJoachimKloubert.CLRToolboxReloaded.NET4_0/Caching/DelegateCache.cs
+++ MarcelJoachimKloubert.CLRToolboxReloaded.NET4_0/Caching/DelegateCache.cs
@@ -1,14 +1,329 @@
-﻿using System;
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. https://github.com/mkloubert/CLRToolboxReloaded
+
+using MarcelJoachimKloubert.CLRToolbox.Data.Conversion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace MarcelJoachimKloubert.CLRToolbox.Caching
 {
     /// <summary>
-    /// 
+    /// Caches delegate operations.
     /// </summary>
-    public partial class DelegateCache
+    public sealed partial class DelegateCache : DisposableObjectBase
     {
+        #region Fields (1)
+
+        private readonly List<CachedItem> _ITEMS = new List<CachedItem>();
+
+        #endregion Fields
+
+        #region Constructors (2)
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegateCache" /> class.
+        /// </summary>
+        /// <param name="syncRoot">The unique object for sync operations.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="syncRoot" /> is <see langword="null" />.
+        /// </exception>
+        public DelegateCache(object syncRoot)
+            : base(syncRoot)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegateCache" /> class.
+        /// </summary>
+        public DelegateCache()
+            : base()
+        {
+        }
+
+        #endregion Constructors
+
+        #region Delegates and Events (2)
+
+        // Delegates (2) 
+
+        /// <summary>
+        /// Describes a cached action.
+        /// </summary>
+        public delegate void CachedAction();
+
+        /// <summary>
+        /// Describes a cached function.
+        /// </summary>
+        /// <returns>The result of the function.</returns>
+        public delegate T CachedFunc<T>();
+
+        #endregion Delegates and Events
+
+        #region Methods (17)
+
+        // Public Methods (11) 
+
+        /// <summary>
+        /// Removes all cached delegates.
+        /// </summary>
+        public void Clear()
+        {
+            lock (this._SYNC)
+            {
+                this._ITEMS.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Invoked an action.
+        /// </summary>
+        /// <param name="action">The action to invoke.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="action" /> is <see langword="null" />.
+        /// </exception>
+        public void InvokeAction(CachedAction action)
+        {
+            this.InvokeInner(action);
+        }
+
+        /// <summary>
+        /// Invoked a function.
+        /// </summary>
+        /// <typeparam name="T">Result type of the function.</typeparam>
+        /// <param name="func">The function to invoke.</param>
+        /// <returns>The result of the function.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="func" /> is <see langword="null" />.
+        /// </exception>
+        public T InvokeFunc<T>(CachedFunc<T> func)
+        {
+            return GlobalConverter.Current
+                                  .ChangeType<T>(this.InvokeInner(func));
+        }
+
+        /// <summary>
+        /// Removes an action from the cache.
+        /// </summary>
+        /// <param name="action">The action to remove.</param>
+        /// <returns>Action was removed or not.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="action" /> is <see langword="null" />.
+        /// </exception>
+        public bool RemoveAction(CachedAction action)
+        {
+            return this.RemoveInner(action);
+        }
+
+        /// <summary>
+        /// Removes a function from the cache.
+        /// </summary>
+        /// <typeparam name="T">Result type of the function.</typeparam>
+        /// <param name="func">The function to remove.</param>
+        /// <returns>Function was removed or not.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="func" /> is <see langword="null" />.
+        /// </exception>
+        public bool RemoveFunc<T>(CachedFunc<T> func)
+        {
+            return this.RemoveInner(func);
+        }
+
+        /// <summary>
+        /// Resets the state of an action.
+        /// </summary>
+        /// <param name="action">The action to reset.</param>
+        /// <returns>Action was resetted or not.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="action" /> is <see langword="null" />.
+        /// </exception>
+        public bool ResetAction(CachedAction action)
+        {
+            return this.ResetInner(action);
+        }
+
+        /// <summary>
+        /// Resets the state of a function.
+        /// </summary>
+        /// <typeparam name="T">Result type of the function.</typeparam>
+        /// <param name="func">The function to reset.</param>
+        /// <returns>Function was resetted or not.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="func" /> is <see langword="null" />.
+        /// </exception>
+        public bool ResetFunc<T>(CachedFunc<T> func)
+        {
+            return this.ResetInner(func);
+        }
+
+        /// <summary>
+        /// Caches an action without a timeout.
+        /// </summary>
+        /// <param name="action">The action to cache.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="action" /> is <see langword="null" />.
+        /// </exception>
+        public void SaveAction(CachedAction action)
+        {
+            this.SaveInner(action, null);
+        }
+
+        /// <summary>
+        /// Caches an action.
+        /// </summary>
+        /// <param name="action">The action to cache.</param>
+        /// <param name="timeout">The timeout / update interval.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="action" /> is <see langword="null" />.
+        /// </exception>
+        public void SaveAction(CachedAction action, TimeSpan timeout)
+        {
+            this.SaveInner(action, timeout);
+        }
+
+        /// <summary>
+        /// Caches a function without a timeout.
+        /// </summary>
+        /// <typeparam name="T">Result type of the function.</typeparam>
+        /// <param name="func">The function to cache.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="func" /> is <see langword="null" />.
+        /// </exception>
+        public void SaveFunc<T>(CachedFunc<T> func)
+        {
+            this.SaveInner(func, null);
+        }
+
+        /// <summary>
+        /// Caches a function without a timeout.
+        /// </summary>
+        /// <typeparam name="T">Result type of the function.</typeparam>
+        /// <param name="func">The function to cache.</param>
+        /// <param name="timeout">The timeout / update interval.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="func" /> is <see langword="null" />.
+        /// </exception>
+        public void SaveFunc<T>(CachedFunc<T> func, TimeSpan timeout)
+        {
+            this.SaveInner(func, timeout);
+        }
+
+        // Protected Methods (1) 
+
+        /// <inheriteddoc />
+        protected override void OnDispose(DisposableObjectBase.DisposeContext ctx)
+        {
+            this._ITEMS
+                .Clear();
+        }
+
+        // Private Methods (5) 
+
+        private object InvokeInner(Delegate @delegate, params object[] args)
+        {
+            if (@delegate == null)
+            {
+                throw new ArgumentNullException("delegate");
+            }
+
+            CachedItem item;
+            lock (this._SYNC)
+            {
+                item = this.TryFindCachedItem(@delegate);
+            }
+
+            if (item != null)
+            {
+                return item.Invoke(args);
+            }
+            else
+            {
+                // not cached => simply invoke
+
+                return @delegate.Method
+                                .Invoke(@delegate.Target,
+                                        args ?? new object[] { null });
+            }
+        }
+
+        private bool RemoveInner(Delegate @delegate)
+        {
+            if (@delegate == null)
+            {
+                throw new ArgumentNullException("delegate");
+            }
+
+            bool result;
+
+            lock (this._SYNC)
+            {
+                CachedItem item = this.TryFindCachedItem(@delegate);
+
+                if (item != null)
+                {
+                    result = this._ITEMS.Remove(item);
+                }
+                else
+                {
+                    result = false;
+                }
+            }
+
+            return result;
+        }
+
+        private bool ResetInner(Delegate @delegate)
+        {
+            if (@delegate == null)
+            {
+                throw new ArgumentNullException("delegate");
+            }
+
+            CachedItem item;
+            lock (this._SYNC)
+            {
+                item = this.TryFindCachedItem(@delegate);
+            }
+
+            if (item != null)
+            {
+                return item.Reset();
+            }
+
+            return false;
+        }
+
+        private void SaveInner(Delegate @delegate, TimeSpan? timeout)
+        {
+            if (@delegate == null)
+            {
+                throw new ArgumentNullException("delegate");
+            }
+
+            lock (this._SYNC)
+            {
+                CachedItem item = this.TryFindCachedItem(@delegate);
+
+                if (item != null)
+                {
+                    item.Timeout = timeout;
+                }
+                else
+                {
+                    this._ITEMS
+                        .Add(new CachedItem(@delegate,
+                                            timeout));
+                }
+            }
+        }
+
+        private CachedItem TryFindCachedItem(Delegate @delegate)
+        {
+            return this._ITEMS
+                       .FirstOrDefault(i => i.Equals(@delegate));
+        }
+
+        #endregion Methods
     }
 }
