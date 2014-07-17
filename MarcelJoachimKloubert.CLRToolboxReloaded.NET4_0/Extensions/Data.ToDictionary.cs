a505b5637d3742fa6cac84d1a4411649091884c4
mkloubert <generalpd@gmx.de>
2014-07-17 16:58:14 +0200

    - ADD: ToDictionary() and ToDictionaryEnumerable() extension methods

---
+++ MarcelJoachimKloubert.CLRToolboxReloaded.NET4_0/Extensions/Data.ToDictionary.cs
@@ -0,0 +1,58 @@
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. https://github.com/mkloubert/CLRToolboxReloaded
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace MarcelJoachimKloubert.CLRToolbox.Extensions
+{
+    static partial class ClrToolboxDataExtensionMethods
+    {
+        #region Methods (2)
+
+        /// <summary>
+        /// Returns an <see cref="IDataRecord" /> as a dictionary.
+        /// </summary>
+        /// <param name="rec">The record from where to get the data from.</param>
+        /// <returns>The record as dictionary.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="rec" /> is <see langword="null" />.
+        /// </exception>
+        public static Dictionary<string, object> ToDictionary(this IDataRecord rec)
+        {
+            return ToDictionary<Dictionary<string, object>>(rec: rec);
+        }
+
+        /// <summary>
+        /// Returns an <see cref="IDataRecord" /> as a dictionary.
+        /// </summary>
+        /// <typeparam name="TDict">Type of the result dictionary.</typeparam>
+        /// <param name="rec">The record from where to get the data from.</param>
+        /// <returns>The record as dictionary.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="rec" /> is <see langword="null" />.
+        /// </exception>
+        public static TDict ToDictionary<TDict>(this IDataRecord rec)
+            where TDict : global::System.Collections.Generic.IDictionary<string, object>, new()
+        {
+            if (rec == null)
+            {
+                throw new ArgumentNullException("rec");
+            }
+
+            var dict = new TDict();
+
+            for (var i = 0; i < rec.FieldCount; i++)
+            {
+                dict.Add(key: rec.GetName(i) ?? string.Empty,
+                         value: rec.IsDBNull(i) ? null : rec.GetValue(i));
+            }
+
+            return dict;
+        }
+
+        #endregion Methods (2)
+    }
+}
---
+++ MarcelJoachimKloubert.CLRToolboxReloaded.NET4_0/Extensions/Data.ToDictionaryEnumerable.cs
@@ -0,0 +1,53 @@
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. https://github.com/mkloubert/CLRToolboxReloaded
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace MarcelJoachimKloubert.CLRToolbox.Extensions
+{
+    static partial class ClrToolboxDataExtensionMethods
+    {
+        #region Methods (2)
+
+        /// <summary>
+        /// Returns an <see cref="IDataReader" /> as a sequence of dictionaries.
+        /// </summary>
+        /// <param name="reader">The reader from where to get the data from.</param>
+        /// <returns>The reader as dictionaries.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="reader" /> is <see langword="null" />.
+        /// </exception>
+        public static IEnumerable<Dictionary<string, object>> ToDictionaryEnumerable(this IDataReader reader)
+        {
+            return ToDictionaryEnumerable<Dictionary<string, object>>(reader: reader);
+        }
+
+        /// <summary>
+        /// Returns an <see cref="IDataReader" /> as a sequence of dictionaries.
+        /// </summary>
+        /// <typeparam name="TDict">Type of the result dictionaries.</typeparam>
+        /// <param name="reader">The reader from where to get the data from.</param>
+        /// <returns>The reader as dictionaries.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="reader" /> is <see langword="null" />.
+        /// </exception>
+        public static IEnumerable<TDict> ToDictionaryEnumerable<TDict>(this IDataReader reader)
+            where TDict : global::System.Collections.Generic.IDictionary<string, object>, new()
+        {
+            if (reader == null)
+            {
+                throw new ArgumentNullException("reader");
+            }
+
+            while (reader.Read())
+            {
+                yield return ToDictionary<TDict>(rec: reader);
+            }
+        }
+
+        #endregion Methods (2)
+    }
+}
