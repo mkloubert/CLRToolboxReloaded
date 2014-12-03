c535d0abcc62352ab7f9f353efd95fd57b2a10fd
mkloubert <generalpd@gmx.de>
2014-12-03 20:44:55 +0100

    - ADD: overwritten Read() extension method for streams - ADD: ReadSingleByte() extension method for Streams

--- MarcelJoachimKloubert.CLRToolboxReloaded.NET4_0/Extensions/IO.Read.cs
+++ MarcelJoachimKloubert.CLRToolboxReloaded.NET4_0/Extensions/IO.Read.cs
@@ -4,25 +4,27 @@
 
 using System;
 using System.IO;
+using System.Linq;
 
 namespace MarcelJoachimKloubert.CLRToolbox.Extensions
 {
     static partial class ClrToolboxExtensionMethods
     {
-        #region Methods (1)
+        #region Methods (2)
 
         /// <summary>
-        /// Reads all data from a stream to a buffer.
+        /// Reads all data from a stream to the beginning of a buffer.
         /// </summary>
         /// <param name="stream">The stream where to read the data from.</param>
         /// <param name="buffer">The buffer that should contain the data.</param>
+        /// <returns>The number of read data.</returns>
         /// <exception cref="ArgumentNullException">
         /// <paramref name="stream" /> and/or <paramref name="buffer" /> are <see langword="null" />.
         /// </exception>
         /// <exception cref="IOException">
         /// <paramref name="stream" /> is not readable.
         /// </exception>
-        /// <remarks>>That operation is done in one read operation.</remarks>
+        /// <remarks>That operation is done in one read operation.</remarks>
         public static int Read(this Stream stream, byte[] buffer)
         {
             if (stream == null)
@@ -43,6 +45,38 @@ namespace MarcelJoachimKloubert.CLRToolbox.Extensions
             return stream.Read(buffer, 0, buffer.Length);
         }
 
-        #endregion Methods (1)
+        /// <summary>
+        /// Tries to read a number of bytes from a stream.
+        /// </summary>
+        /// <param name="stream">The stream from where to read the data from.</param>
+        /// <param name="count">The (maximum) number of data to read.</param>
+        /// <returns>The read data.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="stream" /> is <see langword="null" />.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="count" /> is smaller than 0.
+        /// </exception>
+        /// <exception cref="IOException">
+        /// <paramref name="stream" /> is not readable.
+        /// </exception>
+        public static byte[] Read(this Stream stream, int count)
+        {
+            // do not check 'stream' for (null), this is done
+            // by Read(this Stream, byte[]) method
+
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException("count");
+            }
+
+            var buffer = new byte[count];
+            var bytesRead = Read(stream, buffer);
+
+            return bytesRead == buffer.Length ? buffer
+                                              : buffer.Take(bytesRead).ToArray();
+        }
+
+        #endregion Methods (2)
     }
 }
---
+++ MarcelJoachimKloubert.CLRToolboxReloaded.NET4_0/Extensions/IO.ReadSingleByte.cs
@@ -0,0 +1,43 @@
+﻿// LICENSE: LGPL 3 - https://www.gnu.org/licenses/lgpl-3.0.txt
+
+// s. https://github.com/mkloubert/CLRToolboxReloaded
+
+using System;
+using System.IO;
+
+namespace MarcelJoachimKloubert.CLRToolbox.Extensions
+{
+    static partial class ClrToolboxExtensionMethods
+    {
+        #region Methods (1)
+
+        /// <summary>
+        /// Tries to read a single byte from a stream.
+        /// </summary>
+        /// <param name="stream">The stream from where to read the data from.</param>
+        /// <returns>The read data or <see langword="null" /> if no data is available.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="stream" /> is <see langword="null" />.
+        /// </exception>
+        /// <exception cref="IOException">
+        /// <paramref name="stream" /> is not readable.
+        /// </exception>
+        public static byte? ReadSingleByte(this Stream stream)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException("stream");
+            }
+
+            if (stream.CanRead == false)
+            {
+                throw new IOException();
+            }
+
+            var result = stream.ReadByte();
+            return result >= 0 ? (byte)result : (byte?)null;
+        }
+
+        #endregion Methods (1)
+    }
+}
